4b5aa0eb0500e9827635ab36cbf92b41dfdcfa8c
Quinn Bennett (I) <quinn.bennett@c02m2cn8fd57.corp.aofl.com>
2019-08-16 15:09:03 -0700

    separate file to contain haikus

---
+++ haikus.php
@@ -0,0 +1,91 @@
+<?php
+
+$haikus = array(array(
+"I know something cool
+I bet you don't know what it is
+(today is sunday)",
+"today is monday
+I hope you have a great week
+goodbye then, human",
+"tuesday is quite weird
+that's because it's not green yet
+I hope you're confused",
+"wends-day, wed-nes-day
+no matter what, it's so odd
+I think I'll just leave",
+"thursday might be fine
+I have plans on friday night
+but thursday will work",
+"I'll tell you something
+I was born on a friday
+but that means nothing",
+"what is it they say?
+saturday is for the boys?
+that's a bit sexist"),
+"once there was a bird
+this bird was named Georgina
+that's all I can say",
+"kids can talk so fast
+at least relative to sloths
+time doesn't exist",
+"can you solve problems?
+I will give you a hard task
+please don't screw it up:)",
+"you say I am smart
+I say you are very kind
+but still, you are wrong",
+"you used this program
+very well done sir (or ma'am)
+you are dismissed now",
+"exhausted surfer
+stop browsing the web so late
+go to sleep my dude",
+"silver canopies
+like mystical ceiling fans
+sway in a calm breeze",
+"I shot a nerf gun
+I didn't get in trouble
+it made me quite proud",
+"welcome to 'MUR-CA'
+it should be 'A-MER-I-CA'
+but we say it wrong",
+"you won't believe this
+the author's brother wrote it
+what a funny kid
+
+asked for a haiku
+but got an insult instead
+still, it's a haiku
+
+'no thank you poop head
+I am not as bored as you
+I'm doing fun things'",
+"don't eat too much cake
+deliciously fattening
+but not so healthy",
+"yucky wellness shot
+so much burning in my throat
+I hope it's worth it",
+"croissants are so good
+more than satisfactory
+better with chocolate",
+"I rode bikes today
+I also told lies today
+which statement is true?",
+"rockets are so cool
+space is like the biggest thing
+someday I will go there",
+"mars is a planet
+it dwells within reach of us
+someday you will see",
+"shakespeare wrote poems
+but did that make him famous
+yes it really did",
+"hahahahaha
+hehehehehehehe
+haha so funny",
+"go george harrison
+I believe in your power
+you got this mister");
+
+?>
