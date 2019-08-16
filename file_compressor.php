2e7036a36cf47045821cb074959bff910ff067b0
Quinn Bennett (I) <quinn.bennett@c02m2cn8fd57.corp.aofl.com>
2019-08-16 09:17:06 -0700

    added comments

--- file_compressor.php
+++ file_compressor.php
@@ -8,12 +8,15 @@ $dir_compressed = '/Users/quinn.bennett/Pictures/CompressedFiles/';
 $png_all = shell_exec('find ' . $dir_original.='*.png');
 $png = explode("\n", $png_all);
 
-//compress each file in $png and send compressed files to $dir_compressed
 foreach ($png as $num => $file) {
   if ($num < (count($png)-1)) {
+
+    //compress each file in $png and send compressed files to $dir_compressed
     $filename = basename($file);
     $compressed_file = $dir_compressed . 'compressed_' . $filename;
-    shell_exec('pngquant --quality=60-90 --force - >' . $compressed_file . '<' . $file);
+    shell_exec('pngquant --quality=60-90 --force --skip-if-larger - >' . $compressed_file . '<' . $file);
+
+    //echo original file path
     echo "compressed: \033[1;30m" . $file . " \033[0m";
 
     $gap_a = (strlen($dir_original)+25)-strlen($file);
@@ -21,6 +24,7 @@ foreach ($png as $num => $file) {
       echo " ";
     }
 
+    //echo size of original file
     humanRead_FileSize($file);
 
     $gap_b = (10-strlen($size));
@@ -28,6 +32,7 @@ foreach ($png as $num => $file) {
       echo " ";
     }
 
+    //echo compressed file path
     echo "to: \033[1;34m" . $compressed_file . " \033[0m";
 
     $gap_c = (strlen($dir_compressed)+40)-strlen($compressed_file);
@@ -35,11 +40,13 @@ foreach ($png as $num => $file) {
       echo " ";
     }
 
+    //echo size of compressed file
     humanRead_FileSize($compressed_file);
     echo "\n";
   }
 }
 
+//turn file sizes to human readable
 function humanRead_FileSize($file) {
 global $size;
 $filesize = filesize($file);
